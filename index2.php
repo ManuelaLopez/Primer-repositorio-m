c700d5a14ad1a497078def9e68a321263e6dc69e
ManuelaLopez <mlopez60110@misena.edu.co>
2015-09-02 16:10:15 -0500

    eh terminado

---
+++ index2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es";
+<head>
+<meta charset="UTF-8">
+<title> primera pagina </title>
+</head>
+<body>
+<?php
+    echo 3+3
+    echo "bienvenido ahdjkewhjf";?>
+</body>
+</html>
